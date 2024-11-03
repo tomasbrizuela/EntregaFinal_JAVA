4d8d96acaf40b18942b54b701cc2edb2c4aee859
Tomi <tomasagustinbrizuela@gmial.com>
2024-11-03 17:31:07 -0300

    "Limpieza del main"

--- src/main/java/com/example/universidadSystem/universidad/UniversidadSystemApplication.java
+++ src/main/java/com/example/universidadSystem/universidad/UniversidadSystemApplication.java
@@ -1,20 +1,11 @@
 package com.example.universidadSystem.universidad;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.universidadSystem.universidad.model.Universidad;
-import com.example.universidadSystem.universidad.services.UniversidadService;
-
 @SpringBootApplication
 public class UniversidadSystemApplication implements CommandLineRunner {
-
-	@Autowired
-	UniversidadService universidadService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniversidadSystemApplication.class, args);
@@ -23,16 +14,6 @@ public class UniversidadSystemApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hola Mundo!...");
-
-		List<Universidad> unis = universidadService.getAllUniversidad();
-
-		for (Universidad uni : unis) {
-			System.out.println(uni.getEmail());
-		}
-
-		System.out.println("Http://localhost:9010/universidad/get");
-		System.out.println("Http://localhost:9010/universidad/create");
-		System.out.println("Fin...........");
 
 	}
 
